4860c5a76dad5be70507555dfd65da87864136d1
Md Kaif Manzar <kaifmanzar321@gmail.com>
2024-05-23 11:06:06 +0530

    added ohter controllers for getting series, category, color, productbyid, etc

--- DB/sql.config.js
+++ DB/sql.config.js
@@ -3,16 +3,16 @@ import mysql from 'mysql2'
 export const sqlDatabase = () => {
   // Create a connection to the database using IP address
   const connection = mysql.createConnection({
-    host: "localhost", // e.g., '192.168.1.100'
-    user: "root", // e.g., 'root'
-    password: "Kaifmanzar@321",
-    database: "jung",
+    // host: "localhost", // e.g., '192.168.1.100'
+    // user: "root", // e.g., 'root'
+    // password: "Kaifmanzar@321",
+    // database: "jung",
 
     //m2a
-    // host: "instance1.ch0u2emkohha.ap-south-1.rds.amazonaws.com", // e.g., '192.168.1.100'
-    // user: "admin", // e.g., 'root'
-    // password: "2024Made2Automate",
-    // database: "MainDatabase",
+    host: "instance1.ch0u2emkohha.ap-south-1.rds.amazonaws.com", // e.g., '192.168.1.100'
+    user: "admin", // e.g., 'root'
+    password: "2024Made2Automate",
+    database: "MainDatabase",
     
   });
 
--- app.js
+++ app.js
@@ -72,47 +72,6 @@ app.get("/insertproduct", (req, res) => {
   }
 });
 
-app.get("/getprodcutbyid/:id", (req, res) => {
-  try {
-    const { id } = req.params;
-    console.log(id);
-    const query = `SELECT 
-    product.prod_name, 
-    series.ser_name, 
-    product.photo, 
-    category.cat_name, 
-    color.col_name, 
-    user.user_name, 
-    product.archive, 
-    product.stock, 
-    product.created_at, 
-    product.updated_at
-FROM 
-    product
-JOIN 
-    series ON product.series_id = series.ser_id
-JOIN 
-    category ON product.category_id = category.cat_id
-JOIN 
-    color ON product.color_id = color.col_id
-JOIN 
-    user ON product.user_id = user.user_id
-WHERE 
-    product.prod_id = ${id}`;
-    connection.query(query, (error, result) => {
-      if (error) {
-        return res.json(error);
-      }
-
-      console.log("get the prodcut  successfully:", result);
-      connection.end(); // Close the connection
-
-      return res.json(result);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 app.post("/updateproductbyid", (req, res) => {
   try {
@@ -155,6 +114,119 @@ app.post("/updateproductbyid", (req, res) => {
   } catch (error) {
     console.error("Error:", error);
     return res.status(400).json({ error: error.message });
+  }
+});
+
+
+app.post("/getallseries", async (req, res) => {
+  try {
+    const query = `select distinct series.ser_name, product.series_id from product inner join series on product.series_id = series.ser_id`;
+
+    connection.query(query, (error, result) => {
+      if (error) return res.json(error);
+
+      console.log(result);
+
+      return res.json(result);
+    });
+  } catch (error) {}
+});
+
+app.post("/getallcategoryofaseries", async (req, res) => {
+  try {
+    const { series_id } = req.body;
+    console.log(series_id);
+    const query = `select distinct category.cat_name, product.category_id from product inner join category on product.category_id = category.cat_id where product.series_id = ${series_id}`;
+
+    connection.query(query, (error, result) => {
+      if (error) return res.json(error);
+
+      console.log(result);
+
+      return res.json(result);
+    });
+  } catch (error) {}
+});
+
+app.post("/getallthecolorofacategory", async (req, res) => {
+  try {
+    const { series_id, category_id } = req.body;
+    console.log(series_id, category_id);
+    const query = `
+    SELECT DISTINCT color.col_name, product.color_id
+    FROM product
+    INNER JOIN color ON product.color_id = color.col_id
+    WHERE product.series_id = ${series_id} AND product.category_id = ${category_id}`;
+
+    connection.query(query, (error, result) => {
+      if (error) return res.json(error);
+
+      console.log(result);
+
+      return res.json(result);
+    });
+  } catch (error) {}
+});
+
+app.post("/getallfilterproduct", async(req,res)=>{
+  try {
+    const { series_id, category_id, color_id } = req.body;
+    console.log(series_id, category_id);
+    const query = `
+    SELECT DISTINCT product.prod_id, product.prod_name
+    FROM product
+    INNER JOIN color ON product.color_id = color.col_id
+    WHERE product.series_id = ${series_id} AND product.category_id = ${category_id} AND product.color_id = ${color_id}`;
+
+    connection.query(query, (error, result) => {
+      if (error) return res.json(error);
+
+      console.log(result);
+
+      return res.json(result);
+    });
+  } catch (error) {}
+})
+
+app.get("/getprodcutbyid/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const query = `SELECT 
+    product.prod_name, 
+    series.ser_name, 
+    product.photo, 
+    category.cat_name, 
+    color.col_name, 
+    user.user_name, 
+    product.archive, 
+    product.stock, 
+    product.created_at, 
+    product.updated_at
+FROM 
+    product
+JOIN 
+    series ON product.series_id = series.ser_id
+JOIN 
+    category ON product.category_id = category.cat_id
+JOIN 
+    color ON product.color_id = color.col_id
+JOIN 
+    user ON product.user_id = user.user_id
+WHERE 
+    product.prod_id = ${id}`;
+    connection.query(query, (error, result) => {
+      if (error) {
+        return res.json(error);
+      }
+
+      console.log("get the prodcut  successfully:", result);
+      connection.end(); // Close the connection
+
+      return res.json(result);
+    });
+  } catch (error) {
+    console.log(error);
   }
 });
 
